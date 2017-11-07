e706dfdaed4977325aa226fdb5215ca0c4208a8b
Jeluchu <jesusmarcalderon@gmail.com>
2017-11-07 10:24:13 +0100

    7 Noviembre 2017

---
+++ 2017/00_primitivas/error.cpp
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    fprintf(stderr, "sterr: The world is a vampire.\n")
+
+	return EXIT_SUCCESS;
+}
--- 2017/00_primitivas/javascript/javascript/main.js
+++ 2017/00_primitivas/javascript/javascript/main.js
@@ -1,4 +1,6 @@
 function main(){
+  var stdout = document.getElementById("stdout")
+  stdout.innerHTML += "stdout: The World is a Vampire."  
   alert("The world is a Vampire.")
 
 }
