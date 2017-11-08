c424fac64de45df940c903d412b1b09b65f7036c
Jeluchu <jesusmarcalderon@gmail.com>
2017-11-08 13:09:21 +0100

    Correction of Variadic

--- 2017/00_primitivas/variadic.cpp
+++ 2017/00_primitivas/variadic.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h> //Directivas de proceso
 #include <stdlib.h>
 
 int main(){
@@ -6,7 +6,7 @@ int main(){
     int op1 = 7;
     int op2;
 
-    printf("Opeando 2: ");
+    printf("Operando 2: ");
     scanf(" %i", &op2);
 
     printf("%i + %i = %i\n", op1, op2, op1 + op2);
