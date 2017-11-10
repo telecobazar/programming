4ee2824964a82a5d851663b5300dd4bf3d9d8afd
Jeluchu <jesusmarcalderon@gmail.com>
2017-11-10 14:17:37 +0100

    VariadicDifficult v1.0

--- 2017/00_primitivas/variadicDifficult.cpp
+++ 2017/00_primitivas/variadicDifficult.cpp
@@ -5,39 +5,55 @@ int main(){
 
     int op1;
     int op2;
-    int x;
+    int Opcion;
     int Resultado;
 
+    printf("Bienvenido/a a La calculadora programadora, en ella podrás realizar diversas operaciónes con dos números\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("Introducir el primer número deseado: ");
     scanf(" %i", &op1);
 
     printf("Introducir el segundo número deseado: ");
     scanf(" %i", &op2);
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    fflush(stdin);
 
     printf("¿Qué operación deseas realizar?\n");
     printf("Pulsa 1 si deseas sumar\n");
     printf("Pulsa 2 si deseas restar\n");
     printf("Pulsa 3 si deseas multiplicar\n");
     printf("Pulsa 4 si deseas dividir\n");
-    scanf("%i", &x);
+    printf("Pulsa 0 si deseas salir\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Escoge la opción deseada: ");
 
-    switch('x') {
-    printf("El resultado final de la suma es: ");
+
+do{
+    scanf("%i", &Opcion);
+    switch(Opcion) {
+    case 0:
+           printf("Gracias por usar La Calculadora Programadora, esperamos que te haya sido de utilidad. Muchas gracias\n");
+           return EXIT_SUCCESS;
+    break;
+
     case 1: Resultado = op1+op2;
     break;
 
-    printf("El resultado final de la resta es: ");
     case 2: Resultado = op1-op2;
     break;
 
-   printf("El resultado final de la multiplicación es: ");
     case 3: Resultado = op1*op2;
     break;
 
-   printf("El resultado final de la división es: ");
     case 4: Resultado = op1/op2;
     break;
-    default: printf("%i\n", Resultado);}
 
-    return EXIT_SUCCESS;
+    default: printf("En proceso\n");
+
+  }
+
+    printf("El resultado de la operación es: ");
+    printf("%i\n\n", Resultado);
+    printf("Para realizar otra operación, marque una opción: ");
+ }while(Opcion!=0);
  }
