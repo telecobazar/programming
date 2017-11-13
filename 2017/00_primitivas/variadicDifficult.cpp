70799b94589761d7a4b7753669305af2bd17a739
Jeluchu <jesusmarcalderon@gmail.com>
2017-11-13 09:24:34 +0100

    VariadicDifficult v.1.5 - Included an option to 'Square root'

--- 2017/00_primitivas/variadicDifficult.cpp
+++ 2017/00_primitivas/variadicDifficult.cpp
@@ -8,6 +8,7 @@ int main(){
     int op2;
     int Opcion;
     int Resultado;
+    int n;
 
     printf("Bienvenido/a a La calculadora programadora, en ella podrás realizar diversas operaciónes con dos números\n");
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
@@ -20,14 +21,14 @@ int main(){
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
     fflush(stdin);
 
-    printf("¿Qué operación deseas realizar?\n");
+    printf("\t¿Qué operación deseas realizar?\n");
     printf("\tPulsa 1 si deseas sumar\n");
     printf("\tPulsa 2 si deseas restar\n");
     printf("\tPulsa 3 si deseas multiplicar\n");
     printf("\tPulsa 4 si deseas dividir\n");
     printf("\tPulsa 5 si deseas hacer una Raíz Cuadrada del primer número\n");
     printf("\tPulsa 6 si deseas cambiar los valores\n");
-    printf("\tPulsa 0 si deseas salir\n");
+    printf("\tPulsa 0 si deseas salir\n\n");
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("Escoge la opción deseada: ");
 
@@ -42,26 +43,30 @@ do{
     break;
 
     case 1: Resultado = op1+op2; /* Suma */
-        printf("El resultado de la operación es: ");
+        printf("El resultado de la suma es: ");
         printf("%i\n\n", Resultado);
     break;
 
     case 2: Resultado = op1-op2; /* Resta */
-        printf("El resultado de la operación es: ");
+        printf("El resultado de la resta es: ");
         printf("%i\n\n", Resultado);
     break;
 
     case 3: Resultado = op1*op2; /* Multiplicación */
-        printf("El resultado de la operación es: ");
+        printf("El resultado de la multiplicación es: ");
         printf("%i\n\n", Resultado);
     break;
 
     case 4: Resultado = op1/op2; /* División */
-        printf("El resultado de la operación es: ");
+        printf("El resultado de la división es: ");
         printf("%i\n\n", Resultado);
     break;
 
     case 5: /* Raíz Cuadrada */
+        printf("Introducir el número deseado: ");
+           scanf("%i",&n);
+           n = sqrt(n);
+           printf("El resultado de la raíz cuadrada es: %i\n\n",n);
     break;
 
     case 6:
