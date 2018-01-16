75d5706136b6448d669bdeba8f3c0e6549372bf1
Jeluchu <jesusmarcalderon@gmail.com>
2018-01-16 10:31:13 +0100

    ADD LÍMITE DE OPORTUNIDADEES

--- 2018/Clase/09_estructuras/pregunton.cpp
+++ 2018/Clase/09_estructuras/pregunton.cpp
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 3
+
 int main(void) {
   int numero = 0;
+  int oportunidades = MAX;
 
   do {
     printf("Dime un número del 1 al 3: ");
     scanf(" %i", &numero);
-  } while (numero<1 || numero>3);
+    oportunidades--;
+  } while ( (numero<1 || numero>3) && oportunidades);
 
     printf("\n");
     printf("No preguntes más\n");
