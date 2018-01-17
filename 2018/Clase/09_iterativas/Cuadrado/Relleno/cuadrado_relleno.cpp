83f1984d6f5a72f137ad9d18e1f0ed4e60ed4916
Jeluchu <jesusmarcalderon@gmail.com>
2018-01-17 11:14:50 +0100

    UPDATE FOR CLASS 16/01/18

---
+++ 2018/Clase/09_iterativas/Cuadrado/Relleno/cuadrado_relleno.cpp
@@ -0,0 +1,17 @@
+//PROGRAMA EN C++
+//AUTOR: JESÚS MARÍA CALDERÓN - GITHUB JELUCHU
+//https://github.com/Jeluchu
+
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+
+int main() {
+
+  printf("\n");
+  for (int f=0; f<8; f++)
+  printf("\t* * * * * * * * *\n");
+
+  printf("\n\n");
+  return EXIT_SUCCESS;
+}
