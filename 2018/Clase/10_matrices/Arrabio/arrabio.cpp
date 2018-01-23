a77480b67e064988eab707e4717e3f3f8aa1f692
Jeluchu <jesusmarcalderon@gmail.com>
2018-01-23 10:10:29 +0100

    NEW MATRICES

---
+++ 2018/Clase/10_matrices/Arrabio/arrabio.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 3
+
+int main() {
+
+int a[N][N] = {
+{1, 4, 6},
+{7, 2, 5},
+{9, 8, 3},
+};
+
+for (int desplaz = 0; desplaz < N; desplaz++) {
+  for (int elemen = 0; elemen < N; elemen++)
+    printf("%4i", a[(desplaz+elemen)%N][elemen%N]);
+  printf("\n");
+}
+  return EXIT_SUCCESS;
+}
