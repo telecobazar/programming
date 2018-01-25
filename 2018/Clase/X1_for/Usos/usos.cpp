052f95716b833fed398ed9416e4e673b4076bc67
Jeluchu <jesusmarcalderon@gmail.com>
2018-01-25 23:59:26 +0100

    NUEVA PRÁCTICA USOS

---
+++ 2018/Clase/X1_for/Usos/usos.cpp
@@ -0,0 +1,21 @@
+//PROGRAMA EN C++
+//AUTOR: JESÚS MARÍA CALDERÓN - GITHUB JELUCHU
+//https://github.com/Jeluchu
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+  int i;
+
+// INICIA DESDE CERO, CONTINUA HASTA 10, A LA VEZ VA SUMANDO 1
+  for (i = 0; i<= 10; i++) {
+// EJECUTARÁ ESTE CICLO HASTA QUE "i" TENGA EL VALOR DE 10
+    printf("%i\n", i);
+// SALIDA DEL CILO TRAS QUE i>=10
+  }
+// EJECUTA EL RESTO DE PROGRAMA
+
+  return EXIT_SUCCESS;
+}
