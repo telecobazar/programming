b3a0384e5cfe864ea64b2fa235f5c688685eab26
Jeluchu <jesusmarcalderon@gmail.com>
2018-01-25 23:03:37 +0100

    PRÁCTICAS REFUERZO

---
+++ 2018/Clase/X1_for/TablaMulti/tablamultiplicar.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// PROGRAMA QUE IMPRIMA N TABLA DE MULTIPLICAR
+
+int main() {
+
+  int numero, tabla, resultado;
+
+  printf("Introduce el número: ");
+  scanf("%i", &numero);
+  printf("\n");
+
+  printf("La tabla es la siguiente\n\n");
+
+  // MULTIPLICA - HASTA - SUMANDO DE UNO EN UNO
+  // INICIO     FIN       SUMA 1
+  for(tabla=1; tabla<11; tabla++) {
+
+    resultado=numero*tabla;
+    printf("%i x %i = %i\n", numero, tabla, resultado );
+  }
+  printf("\n");
+  
+  return EXIT_SUCCESS;
+}
