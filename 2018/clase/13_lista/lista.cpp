5d4a73d9ce1d4d967cc166e90e8cf40537273ec4
Jesús Mª <jeluchu@gmail.com>
2018-02-23 14:32:18 +0100

    LASTED UPDATE 23/02/18

--- 2018/clase/13_lista/lista.cpp
+++ 2018/clase/13_lista/lista.cpp
@@ -21,22 +21,36 @@
 
 */
 
+
+lee_un_nombre_y_cargalo{
+  char buffer[N]; // LA DIRECCIÓN DEL PRIMER CARÁCTER DE LA RESERVA - TAMBIÉN SE PUEDE PONER char *buffer;
+  int len;
+  char *algun_sitio = NULL;
+
+  printf("Dime tu nombre: ");
+  fgets(buffer, N, stdin); //RECOGE LO QUE EL USUARIO ESCRIBE
+  len = strlen(buffer); //strlen FUNCIÓN QUE DEVUELVE EL VALOR REAL, NO ADMITE '\0', SÍ:  PEPE = 4 - NO:  PEPE\0 = 5
+  algun_sitio = (char *) malloc( len * sizeof(char));
+  strncpy(algun_sitio, buffer, len - 1);
+  algun_sitio[len-1] = '\0';
+
+  return algun_sitio;
+
+}
+
+
 int main(){
 
     char **lista = NULL; //CUIDADO CON LOS  PUNTEROS SALVAJES
-    char buffer[N]; // LA DIRECCIÓN DEL PRIMER CARÁCTER DE LA RESERVA - TAMBIÉN SE PUEDE PONER char *buffer;
-    int len;
-    char *algun_sitio = NULL;
+
+
     bool hasta_nueva_orden = true;
 
     while(hasta_nueva_orden){
-        printf("Dime tu nombre: ");
-        fgets(buffer, N, stdin); //RECOGE LO QUE EL USUARIO ESCRIBE
-        len = strlen(buffer); //strlen FUNCIÓN QUE DEVUELVE EL VALOR REAL, NO ADMITE '\0', SÍ:  PEPE = 4 - NO:  PEPE\0 = 5
-        algun_sitio = (char *) malloc( len * sizeof(char));
-        strncpy(algun_sitio, buffer, len - 1);
-        algun_sitio[len-1] = '\0';
+
         hasta_nueva_orden = false;
+
+        algun_sitio = lee_un_nombre_y_cargalo();
     }
     free(algun_sitio);
 
