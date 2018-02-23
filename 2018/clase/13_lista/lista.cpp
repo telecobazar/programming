70c6f16587de63685ab577e5fb9efc0de5731767
Jesús Mª <jeluchu@gmail.com>
2018-02-23 14:19:22 +0100

    FIX STRNCPY

--- 2018/clase/13_lista/lista.cpp
+++ 2018/clase/13_lista/lista.cpp
@@ -26,16 +26,19 @@ int main(){
     char **lista = NULL; //CUIDADO CON LOS  PUNTEROS SALVAJES
     char buffer[N]; // LA DIRECCIÓN DEL PRIMER CARÁCTER DE LA RESERVA - TAMBIÉN SE PUEDE PONER char *buffer;
     int len;
+    char *algun_sitio = NULL;
+    bool hasta_nueva_orden = true;
 
     while(hasta_nueva_orden){
         printf("Dime tu nombre: ");
         fgets(buffer, N, stdin); //RECOGE LO QUE EL USUARIO ESCRIBE
         len = strlen(buffer); //strlen FUNCIÓN QUE DEVUELVE EL VALOR REAL, NO ADMITE '\0', SÍ:  PEPE = 4 - NO:  PEPE\0 = 5
         algun_sitio = (char *) malloc( len * sizeof(char));
-        strcpy(algun_sitio, buffer, len - 1);
+        strncpy(algun_sitio, buffer, len - 1);
         algun_sitio[len-1] = '\0';
+        hasta_nueva_orden = false;
     }
-
+    free(algun_sitio);
 
     return EXIT_SUCCESS;
 }
