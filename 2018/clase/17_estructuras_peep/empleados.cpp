69217f16de45676cf8c5538cb5efb7bb6cc9d2e4
Jesús Mª <jeluchu@gmail.com>
2018-04-24 09:45:34 +0200

    EMPLEADOS WITH BUGS

---
+++ 2018/clase/17_estructuras_peep/empleados.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 6
+#define PLANTILLA 2 
+typedef struct {
+  int id;
+  int saldo;
+  double sueldo;
+  char name[N];
+} Empleado;
+
+void rellena(Empleado *nuevo){
+
+  printf("Nombre: ");
+  scanf(" %s", nuevo->name);
+
+  printf("Saldo: ");
+  scanf(" %i", nuevo->saldo);
+
+}
+
+void imprime(Empleado persona) {
+
+  printf("Nombre : %s\n"
+         "Saldo: %s\n\n", persona.name, persona.saldo);
+  /* code */
+}
+
+int main() {
+
+  Empleado persona[PLANTILLA];
+
+  for (int i = 0; i < PLANTILLA; i++)
+    rellena(&persona[i]);
+
+  for (int i = 0; i < PLANTILLA; i++)
+    imprime(persona[i]);
+
+  printf("\n");
+
+  return EXIT_SUCCESS;
+}
